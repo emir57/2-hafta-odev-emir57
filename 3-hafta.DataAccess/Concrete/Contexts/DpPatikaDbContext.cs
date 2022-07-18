5bc291217204397d4325fb69afa7e8cb586a5dc2
emir57 <emir.gurbuz06@hotmail.com>
2022-07-18 23:54:49 +0300

    DbContexts

---
+++ 3-hafta.DataAccess/Concrete/Contexts/DpPatikaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+namespace _3_hafta.DataAccess.Concrete.Contexts
+{
+    public class DpPatikaDbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DpPatikaDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IDbConnection CreateConnection()
+        {
+            return new NpgsqlConnection(_configuration.GetConnectionString("PostgreSqlConnection"));
+        }
+    }
+}
---
+++ 3-hafta.DataAccess/Concrete/Contexts/EfPatikaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using _3_hafta.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace _3_hafta.DataAccess.Concrete.Contexts
+{
+    public class EfPatikaDbContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Folder> Folder { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Country> Country { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=123;Host=localhost;Port=5432;Database=PATIKA;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;");
+        }
+    }
+}
