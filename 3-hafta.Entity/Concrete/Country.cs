f095125ac0f03999f592f0c2e1e9e400eb0c9578
emir57 <emir.gurbuz06@hotmail.com>
2022-07-18 23:29:04 +0300

    Country Entity

---
+++ 3-hafta.Entity/Concrete/Country.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace _3_hafta.Entity.Concrete
+{
+    public class Country : IEntity
+    {
+        [Key]
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string Continent { get; set; }
+        public string Currency { get; set; }
+    }
+}
