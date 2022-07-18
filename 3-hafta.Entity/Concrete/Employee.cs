a4edeef8b9a13b7bdac6a127082bf8b0a2f5a137
emir57 <emir.gurbuz06@hotmail.com>
2022-07-18 23:27:22 +0300

    Employee Entity

---
+++ 3-hafta.Entity/Concrete/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace _3_hafta.Entity.Concrete
+{
+    public class Employee : IEntity
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int DeptId { get; set; }
+    }
+}
