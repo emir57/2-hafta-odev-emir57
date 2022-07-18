e0b4aa2e543c2bf9ca44e1978979762001f07525
emir57 <emir.gurbuz06@hotmail.com>
2022-07-18 23:07:41 +0300

    IEntityRepository

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        Task<bool> AddAsync(TEntity entity);
+        Task<bool> UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(TEntity entity);
+
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+    }
+}
