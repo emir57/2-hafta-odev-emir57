5b5d1ce623521a4aec100e2ad5790422e3a5b5f2
emir57 <emir.gurbuz06@hotmail.com>
2022-07-18 23:30:37 +0300

    Department Entity

---
+++ 3-hafta.Entity/Concrete/Department.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace _3_hafta.Entity.Concrete
+{
+    public class Department : IEntity
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DeptName { get; set; }
+        public int CountryId { get; set; }
+    }
+}
