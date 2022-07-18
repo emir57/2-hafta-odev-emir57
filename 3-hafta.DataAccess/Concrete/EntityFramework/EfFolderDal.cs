4fdee78af23d45be188de7dfa5d5ba1570fb97a9
emir57 <emir.gurbuz06@hotmail.com>
2022-07-19 00:06:52 +0300

    Create EntityFramework Dals

---
+++ 3-hafta.DataAccess/Concrete/EntityFramework/EfFolderDal.cs
@@ -0,0 +1,11 @@
+﻿using _3_hafta.DataAccess.Abstract;
+using _3_hafta.DataAccess.Concrete.Contexts;
+using _3_hafta.Entity.Concrete;
+using Core.DataAccess.EntityFramework;
+
+namespace _3_hafta.DataAccess.Concrete.EntityFramework
+{
+    public class EfFolderDal : EfEntityRepositoryBase<Folder, EfPatikaDbContext>, IFolderDal
+    {
+    }
+}
