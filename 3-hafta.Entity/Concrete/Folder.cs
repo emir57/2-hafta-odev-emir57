55c0057a0b3a4b54cd353d757b3c631cea8cb314
emir57 <emir.gurbuz06@hotmail.com>
2022-07-18 23:32:03 +0300

    Folder Entity

---
+++ 3-hafta.Entity/Concrete/Folder.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace _3_hafta.Entity.Concrete
+{
+    public class Folder : IEntity
+    {
+        [Key]
+        public int FolderId { get; set; }
+        public int EmpId { get; set; }
+        public string AccessType { get; set; }
+    }
+}
