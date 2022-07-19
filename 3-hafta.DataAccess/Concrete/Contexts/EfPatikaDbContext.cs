942aef481d46f53c7fdbbf57820e43cb81634604
emir57 <emir.gurbuz06@hotmail.com>
2022-07-19 12:42:10 +0300

    IResult and Result

--- 3-hafta.DataAccess/Concrete/Contexts/EfPatikaDbContext.cs
+++ 3-hafta.DataAccess/Concrete/Contexts/EfPatikaDbContext.cs
@@ -8,8 +8,8 @@ namespace _3_hafta.DataAccess.Concrete.Contexts
     {
         public DbSet<Employee> Employee { get; set; }
         public DbSet<Folder> Folder { get; set; }
-        public DbSet<Department> Department { get; set; }
-        public DbSet<Country> Country { get; set; }
+        //public DbSet<Department> Department { get; set; }
+        //public DbSet<Country> Country { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql(getConnectionString("PostgreSqlConnection"));
