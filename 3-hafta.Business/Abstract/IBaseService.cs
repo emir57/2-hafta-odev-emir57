7c3aad66187debcde25a33cb9fe0c7822c634979
emir57 <emir.gurbuz06@hotmail.com>
2022-07-19 12:55:30 +0300

    Create IBaseService

--- 3-hafta.Business/Abstract/IBaseService.cs
+++ 3-hafta.Business/Abstract/IBaseService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Utilities.Result;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace _3_hafta.Business.Abstract
 {
     public interface IBaseService<TEntity, TDto>
     {
+        Task<IResult> AddAsync(TEntity entity);
+        Task<IResult> UpdateAsync(TEntity entity);
+        Task<IResult> DeleteAsync(TEntity entity);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities);
+        Task DeleteRangeAsync(IEnumerable<TEntity> entities);
+
+        Task<IDataResult<List<TDto>>> GetListAsync();
+        Task<IDataResult<TDto>> GetByIdAsync(int id);
     }
 }
