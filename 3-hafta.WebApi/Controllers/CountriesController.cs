d25dbbc95db0a858a83bfcdb99c217de425a2273
emir57 <emir.gurbuz06@hotmail.com>
2022-07-19 13:48:17 +0300

    CountriesController

--- 3-hafta.WebApi/Controllers/CountriesController.cs
+++ 3-hafta.WebApi/Controllers/CountriesController.cs
@@ -1,4 +1,6 @@
-﻿using _3_hafta.DataAccess.Abstract;
+﻿using _3_hafta.Business.Abstract;
+using _3_hafta.DataAccess.Abstract;
+using _3_hafta.Dto.Concrete;
 using _3_hafta.Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,20 +8,39 @@ namespace _3_hafta.WebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class CountriesController : ControllerBase
+    public class CountriesController : BaseController<Country, CountryDto>
     {
-        private readonly ICountryDal _countryDal;
-
-        public CountriesController(ICountryDal countryDal)
+        public CountriesController(ICountryService service) : base(service)
         {
-            _countryDal = countryDal;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] Country country)
+        public async Task<IActionResult> Post([FromBody] CountryDto countryDto)
         {
-            var result = await _countryDal.AddAsync(country);
-            return Ok(result);
+            return await base.AddAsync(countryDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] CountryDto countryDto)
+        {
+            return await base.UpdateAsync(id, countryDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            return await base.DeleteAsync(id);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return await base.GetListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return await base.GetByIdAsync(id);
         }
 
     }
