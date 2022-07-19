f04986fe4e3426d26e28218aee50b642a72af67b
emir57 <emir.gurbuz06@hotmail.com>
2022-07-19 13:51:16 +0300

    Create BusinessMapper

---
+++ 3-hafta.Business/Mapper/BusinessMapper.cs
@@ -0,0 +1,17 @@
+﻿using _3_hafta.Dto.Concrete;
+using _3_hafta.Entity.Concrete;
+using AutoMapper;
+
+namespace _3_hafta.Business.Mapper
+{
+    public class BusinessMapper : Profile
+    {
+        public BusinessMapper()
+        {
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Employee, EmployeeDto>().ReverseMap();
+            CreateMap<Department, DepartmentDto>().ReverseMap();
+            CreateMap<Folder, FolderDto>().ReverseMap();
+        }
+    }
+}
