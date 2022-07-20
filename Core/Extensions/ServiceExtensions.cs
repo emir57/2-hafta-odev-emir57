a8fdd960ef40a02246c2dd1e6ef13712c274247b
emir57 <emir.gurbuz06@hotmail.com>
2022-07-20 20:09:26 +0300

    Interceptors

---
+++ Core/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection
+            AddDependencyResolvers(this IServiceCollection services, params ICoreModule[] modules)
+        {
+            foreach (var module in modules)
+                module.Load(services);
+            return ServiceTool.Create(services);
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,39 @@
+﻿using Castle.DynamicProxy;
+
+namespace Core.Utilities.Interceptors
+{
+    public class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation, System.Exception e) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            bool success = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+                var task = invocation.ReturnValue as Task;
+                if (task != null)
+                {
+                    OnException(invocation, task.Exception);
+                    success = false;
+                }
+            }
+            catch (System.Exception e)
+            {
+                OnException(invocation, e);
+                success = false;
+            }
+            finally
+            {
+                if (success)
+                    OnSuccess(invocation);
+            }
+            OnAfter(invocation);
+        }
+    }
+}
