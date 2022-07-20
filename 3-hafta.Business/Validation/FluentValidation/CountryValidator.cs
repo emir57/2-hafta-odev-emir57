cf01ec5c1019a57249d12f433bb72e68157b135b
emir57 <emir.gurbuz06@hotmail.com>
2022-07-20 18:52:18 +0300

    DepartmentValidator

--- 3-hafta.Business/Validation/FluentValidation/CountryValidator.cs
+++ 3-hafta.Business/Validation/FluentValidation/CountryValidator.cs
@@ -13,24 +13,24 @@ namespace _3_hafta.Business.Validation.FluentValidation
         public CountryValidator()
         {
             RuleFor(c => c.CountryName)
-                .Null()
-                .Empty()
+                .NotNull()
+                .NotEmpty()
                 .WithMessage("Şehir adı boş olamaz");
             RuleFor(c => c.CountryName)
                 .MaximumLength(50)
                 .WithMessage("Şehir adı maksimum 50 karakter olabilir");
 
             RuleFor(c => c.Continent)
-                .Null()
-                .Empty()
+                .NotNull()
+                .NotEmpty()
                 .WithMessage("Kıta boş olamaz");
             RuleFor(c => c.Continent)
                 .MaximumLength(50)
                 .WithMessage("Kıta maksimum 50 karakter olabilir");
 
             RuleFor(c => c.Currency)
-                .Null()
-                .Empty()
+                .NotNull()
+                .NotEmpty()
                 .WithMessage("Para birimi boş olamaz");
             RuleFor(c => c.Currency)
                 .MaximumLength(3)
---
+++ 3-hafta.Business/Validation/FluentValidation/DepartmentValidator.cs
@@ -0,0 +1,26 @@
+﻿using _3_hafta.Dto.Concrete;
+using FluentValidation;
+
+namespace _3_hafta.Business.Validation.FluentValidation
+{
+    public class DepartmentValidator : AbstractValidator<DepartmentDto>
+    {
+        public DepartmentValidator()
+        {
+            RuleFor(x => x.DeptName)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Departman adı boş olamaz");
+
+            RuleFor(d => d.DeptName)
+                .MaximumLength(50)
+                .WithMessage("Departman adı maksimum 50 karakter olabilir");
+
+            RuleFor(d => d.CountryId)
+                .NotNull()
+                .NotEmpty()
+                .NotEqual(0)
+                .WithMessage("Şehir id boş olamaz");
+        }
+    }
+}
